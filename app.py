2b12dfe4cce38c7cc8a3b6c9bbe4b81cf3156f15
kumarnitish378 <nitish.ns378@gmail.com>
2025-05-24 02:21:29 +0530

    database fixed

--- app.py
+++ app.py
@@ -94,6 +94,6 @@ def submit_contact():
     return jsonify({"status": "success"}), 201
 
 if __name__ == '__main__':
-    init_db()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    if not os.path.exists(DB_NAME):
+        init_db()
+    app.run(host='0.0.0.0', port=5000, debug=True)
